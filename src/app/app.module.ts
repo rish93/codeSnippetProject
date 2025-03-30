bd88b9017ceaa1c604c2ae3d797a8c5ee07ff643
rish93 <rish93@gmail.com>
2025-03-30 14:21:24 +0530

    updted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatButtonModule } from '@angular/material/button';
 
@@ -16,25 +16,19 @@ const json = require('highlight.js/lib/languages/json');
 
 hljs.registerLanguage('json', json);
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    MatTabsModule,
-    MatButtonModule,
-    HighlightModule,
-  ],
-  providers: [
-    {
-      provide: HIGHLIGHT_OPTIONS,
-      useValue: { coreLibraryLoader: () => import('highlight.js') }
-    }
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        MatTabsModule,
+        MatButtonModule,
+        HighlightModule], providers: [
+        {
+            provide: HIGHLIGHT_OPTIONS,
+            useValue: { coreLibraryLoader: () => import('highlight.js') }
+        },
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule { }

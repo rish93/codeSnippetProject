82a665a91f3b5c6a759922412e9943aafbdcb13a
rish93 <rish93@gmail.com>
2025-03-30 13:08:41 +0530

    iniital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'codeSnippetProject';
+
+  codeSnippet = `fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json));`;
+
+apiResponse: any;
+
+constructor(private http: HttpClient) {}
+
+fetchData() {
+  this.http.get('https://jsonplaceholder.typicode.com/todos/1')
+    .subscribe(response => this.apiResponse = response);
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonModule } from '@angular/material/button';
+
+// Import Highlight.js
+import { HighlightModule, HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
+import typescript from 'highlight.js/lib/languages/typescript';
+const hljs = require('highlight.js/lib/core');
+const json = require('highlight.js/lib/languages/json');
+
+hljs.registerLanguage('json', json);
 
 @NgModule({
   declarations: [
@@ -11,10 +22,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    MatTabsModule,
+    MatButtonModule,
+    HighlightModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HIGHLIGHT_OPTIONS,
+      useValue: { coreLibraryLoader: () => import('highlight.js') }
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
